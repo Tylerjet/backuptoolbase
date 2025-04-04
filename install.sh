f3da8904f782fdf99c6bb00f65ec1dd31956d646
MrTylerjet <MrTylerjet@gmail.com>
2025-04-04 04:45:17 -0600

    another compare statement

--- install.sh
+++ install.sh
@@ -131,7 +131,7 @@ configure() {
                     continue
                 fi
                 ghusername=$(getUsername "$ghtoken")
-                if [ -z "$ghusername" ]; then
+                if [ -z "$ghusername" ] || [ $username -eq 1 ]; then
                     whiptail --msgbox "Invalid GitHub token or unable to contact GitHub API. Please check your connection and try again!" 10 76
                     unset ghtoken
                     continue
