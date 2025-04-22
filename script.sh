1f7ef9e5344f8ebd876a567c4ebce4228f1335bd
MrTylerjet <MrTylerjet@gmail.com>
2025-04-22 02:35:27 -0600

    fix my merge messup

--- script.sh
+++ script.sh
@@ -110,6 +110,15 @@ createBackupFolder() {
 }
 
 checkEnv() {
+    # # Check if .env is v1 version
+    # if [[ ! -v backupPaths ]]; then
+    #     echo ".env file is not using version 2 config, upgrading to V2"
+    #     if bash $parent_path/utils/v1convert.sh; then
+    #         echo "Upgrade complete restarting script.sh"
+    #         sleep 2.5
+    #         exec "$parent_path/script.sh" "$args"
+    #     fi
+    # fi
     # Check if username is defined in .env
     if [[ "$commit_username" != "" ]]; then
         git config user.name "$commit_username"
